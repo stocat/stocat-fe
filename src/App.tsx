9910d65aa27317e77f8cac0f272417be6759c094
2ssac <ivory.ma9ic@gmail.com>
2025-09-12 02:24:05 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>Welcome to Stocat World!</div>
+      <div className="w-full h-screen mt-20 flex flex-col justify-start items-center">
+        <p className="text-4xl text-gray-300 hover:text-gray-500 active:text-gray-800">
+          Welcome to Stocat World!
+        </p>
+      </div>
     </>
   );
 }
