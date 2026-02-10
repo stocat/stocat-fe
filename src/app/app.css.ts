de9dd8cb81cb460dc0062dda3c4b93293844c4b3
KAI <tmdcks1103@gachon.ac.kr>
2026-02-10 23:55:28 +0900

    :sparkles: feat: 배경 색 수정

--- src/app/app.css.ts
+++ src/app/app.css.ts
@@ -1,11 +1,12 @@
 import { style } from "@vanilla-extract/css";
+import { vars } from "@/shared/styles/vars.css";
 
 export const viewport = style({
   minHeight: "100vh",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  backgroundColor: "black",
+  backgroundColor: vars.color.grey[100],
 });
 
 export const mobileFrame = style({
@@ -20,4 +21,5 @@ export const mobileFrame = style({
 export const content = style({
   flex: 1,
   overflowY: "auto",
+  paddingBottom: "64px",
 });
