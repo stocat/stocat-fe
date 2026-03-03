d7dea47e9e67a91d693322456bce6a85969fe6ab
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-03-03 15:55:37 +0900

    feat: Medal 아이콘 컴포넌트 추가 및 HomeCoin SVG 제거
    - MedalFirst/Second/Third SVG 아이콘 컴포넌트 추가
    - 미사용 HomeCoinOne/Two SVG 파일 제거
    - icons index에 Medal 아이콘 export 추가

--- src/assets/icons/components/index.ts
+++ src/assets/icons/components/index.ts
@@ -1,7 +1,9 @@
-// src/pages/home/components/HomeGreetingSection
 export { default as HomeCoinOne } from "./HomeCoinOne";
 export { default as HomeCoinTwo } from "./HomeCoinTwo";
 
 export { default as CharacterSnovy } from "./CharacterSnovy";
 export { default as VectorRight } from "./VectorRight";
 export { default as AiSparkle } from "./AiSparkle";
+export { default as MedalFirst } from "./MedalFirst";
+export { default as MedalSecond } from "./MedalSecond";
+export { default as MedalThird } from "./MedalThird";
